ca265ef7f73229703d33488b8a0484e8b304d9d7
William Boggs <david@wbanalytics.dev>
2025-05-05 12:58:54 -0500

    Add files via upload
    slight update

--- script.js
+++ script.js
@@ -12,50 +12,61 @@ class Node {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = 8;
+    this.radius = 12;
   }
 
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = '#0ff';
+    ctx.fillStyle = '#00ffff';
+    ctx.shadowColor = '#00ffff';
+    ctx.shadowBlur = 10;
     ctx.fill();
+    ctx.shadowBlur = 0;
   }
 }
 
-const layers = [3, 5, 4, 2];
+const layers = [3, 5, 2];
 const nodes = [];
 
-const spacingX = canvas.width / (layers.length + 1);
-for (let i = 0; i < layers.length; i++) {
-  const layer = [];
-  const spacingY = canvas.height / (layers[i] + 1);
-  for (let j = 0; j < layers[i]; j++) {
-    layer.push(new Node(spacingX * (i + 1), spacingY * (j + 1)));
+function setupNodes() {
+  const layerWidth = canvas.width / (layers.length + 1);
+  for (let i = 0; i < layers.length; i++) {
+    const layer = [];
+    const layerHeight = canvas.height / (layers[i] + 1);
+    for (let j = 0; j < layers[i]; j++) {
+      const x = layerWidth * (i + 1);
+      const y = layerHeight * (j + 1);
+      layer.push(new Node(x, y));
+    }
+    nodes.push(layer);
   }
-  nodes.push(layer);
 }
+setupNodes();
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Draw connections
-  ctx.strokeStyle = '#0a0';
-  nodes.forEach((layer, i) => {
-    if (i < nodes.length - 1) {
-      layer.forEach(node => {
-        nodes[i + 1].forEach(nextNode => {
-          ctx.beginPath();
-          ctx.moveTo(node.x, node.y);
-          ctx.lineTo(nextNode.x, nextNode.y);
-          ctx.stroke();
-        });
-      });
+  // Draw lines
+  ctx.lineWidth = 1;
+  for (let i = 0; i < nodes.length - 1; i++) {
+    for (const from of nodes[i]) {
+      for (const to of nodes[i + 1]) {
+        ctx.beginPath();
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y);
+        ctx.strokeStyle = '#88ffff33';
+        ctx.stroke();
+      }
     }
-  });
+  }
 
   // Draw nodes
-  nodes.flat().forEach(node => node.draw());
+  for (const layer of nodes) {
+    for (const node of layer) {
+      node.draw();
+    }
+  }
 
   requestAnimationFrame(animate);
 }
